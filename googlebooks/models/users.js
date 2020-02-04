50cf2b26152956b84a8495fab78159a9bd9508ca
Sheldon Perkins <csperkins18@gmail.com>
2020-02-04 14:11:18 -0500

    MONGOURI connect

---
+++ googlebooks/models/users.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcrypt');
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks";
+mongoose.connect(MONGODB_URI);
+mongoose.connection.once('open', () => {
+    console.log('connected with mongo ORM')
+})
+
